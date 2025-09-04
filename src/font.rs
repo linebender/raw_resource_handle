02ae084f7e2fc9d444a16992760c0af9db94859b
Nico Burns <nico@nicoburns.com>
2025-09-04 12:59:34 +0100

    Rename Font to RawFontData (#5)
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/font.rs
+++ src/font.rs
@@ -5,14 +5,14 @@ use crate::Blob;
 
 /// Owned shareable font resource.
 #[derive(Clone, PartialEq, Debug)]
-pub struct Font {
+pub struct FontData {
     /// Blob containing the content of the font file.
     pub data: Blob<u8>,
     /// Index of the font in a collection, or 0 for a single font.
     pub index: u32,
 }
 
-impl Font {
+impl FontData {
     /// Creates a new font with the given data and collection index.
     #[must_use]
     pub fn new(data: Blob<u8>, index: u32) -> Self {
