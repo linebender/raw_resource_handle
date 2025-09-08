e98cba89dbf9b77313d573b790babdc1ffb55486
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2025-09-08 16:40:40 +0100

    Update the lint set

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,16 @@
 //! This crate is not intended for long-term use, and we expect our resource handling story to change.
 //! That's the reason that this crate has the organisation name ("Linebender") in its crate name; we avoid squatting a more general name after we abandon it.
 
-// LINEBENDER LINT SET - lib.rs - v1
+// LINEBENDER LINT SET - lib.rs - v3
 // See https://linebender.org/wiki/canonical-lints/
-// These lints aren't included in Cargo.toml because they
-// shouldn't apply to examples and tests
-#![warn(unused_crate_dependencies)]
+// These lints shouldn't apply to examples or tests.
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+// These lints shouldn't apply to examples.
 #![warn(clippy::print_stdout, clippy::print_stderr)]
+// Targeting e.g. 32-bit means structs containing usize can give false positives for 64-bit.
+#![cfg_attr(target_pointer_width = "64", warn(clippy::trivially_copy_pass_by_ref))]
 // END LINEBENDER LINT SET
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![no_std]
 
 #[cfg(feature = "std")]
 // Ensure that we don't compile if you're using the std feature on a platform without `std`
